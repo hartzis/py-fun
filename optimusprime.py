935ca1c57e0008462bf308ed5af68f32cec53487
Brian Hartz <brianhartz@gmail.com>
2013-11-19 23:39:53 -0700

    Update Comments
    Describe the actual testing of the number

--- optimusprime.py
+++ optimusprime.py
@@ -10,28 +10,32 @@
 #-------------------------------------------------------------------------------
 
 def optimusPrime(test_num):
-    #test if number is even a number let alone an integer
+    # test if number is even a number let alone an integer
     if not type(test_num) == int:
         print "Not a valid input integer"
         return
-    #check if number is divisible by 2 and/or <=7 or zero,
-    #then create list for prime test
+    # check if number is divisible by 2 or <=7 or zero,
+    # then create list of numbers for prime testing, explained below
     if test_num in (0,1):
         print "Number {} is not a prime".format(test_num)
         return
     elif test_num in (2,3,5,7):
         print "Number {} is a prime".format(test_num)
         return
+    # see prime test below for explination
     elif test_num % 2 == 0:
         print "Number {} is not a prime, divisible by 2".format(test_num)
         return
     else:
+        # Create a list of all numbers that are less than (test_num/2)
         test_list = range((test_num/2))
-    #reverse list for prime test
+    # reverse list for prime test
     test_list.reverse()
 
-    #prime test: check the remainder of each division to see if number is a prime
-    #if it gets all the way to 1 it is a prime
+    # prime test: Loop through all numbers in test_list, and if the remainder of
+    # test_num / test_list is equal to zero, then it is not a prime, unless the
+    # list gets all the way to 1, then it is a prime. All of this is quickly
+    # calculated using the amazing modulo operator '%'
     for i in test_list:
         if test_num % (i) == 0:
             if i == 1:
